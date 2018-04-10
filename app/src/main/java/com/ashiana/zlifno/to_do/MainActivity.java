aa79289bf3f633d4c22378041fe91724e5429849
kunaalkumar <kunaal@ashiana.com>
2018-04-09 21:00:24 -0600

    Initial commit

---
+++ app/src/main/java/com/ashiana/zlifno/to_do/MainActivity.java
@@ -0,0 +1,35 @@
+package com.ashiana.zlifno.to_do;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<Note> notes = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.notes_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new NotesAdapter(notes);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+}
