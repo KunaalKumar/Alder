26f7e4c242d0075f17b5616051a86ceaabc69944
kunaalkumar <kunaal@ashiana.com>
2018-04-11 10:21:08 -0600

    Add NoteContract for database Add NoteDBHelper for database access Add changes for debugging (To be removed later) Change main xml layout for debugging (Tobe removed later) Update colors.xml

--- app/src/main/java/com/ashiana/zlifno/to_do/MainActivity.java
+++ app/src/main/java/com/ashiana/zlifno/to_do/MainActivity.java
@@ -1,10 +1,16 @@
 package com.ashiana.zlifno.to_do;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import com.ashiana.zlifno.to_do.data.NoteContract;
+import com.ashiana.zlifno.to_do.data.NoteDBHelper;
 
 import java.util.ArrayList;
 
@@ -19,17 +25,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayDatabaseInfo();
+//        ArrayList<Note> notes = new ArrayList<>();
+//
+//        recyclerView = findViewById(R.id.notes_recycler_view);
+//        recyclerView.setHasFixedSize(true);
+//
+//        layoutManager = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(layoutManager);
+//
+//        adapter = new NotesAdapter(notes);
+//        recyclerView.setItemAnimator(new DefaultItemAnimator());
+//        recyclerView.setAdapter(adapter);
+    }
 
-        ArrayList<Note> notes = new ArrayList<>();
+    // Just for testing
+    private void displayDatabaseInfo() {
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+        NoteDBHelper dbHelper = new NoteDBHelper(this);
 
-        recyclerView = findViewById(R.id.notes_recycler_view);
-        recyclerView.setHasFixedSize(true);
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        adapter = new NotesAdapter(notes);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(adapter);
+//         Perform this raw SQL query "SELECT * FROM pets"
+//         to get a Cursor that contains all rows from the pets table.
+        Cursor cursor = db.rawQuery("SELECT * FROM " + NoteContract.NoteEntry.TABLE_NAME, null);
+        try {
+            // Display the number of rows in the Cursor (which reflects the number of rows in the
+            // pets table in the database).
+            TextView displayView = findViewById(R.id.text_view);
+            displayView.setText("Number of rows in pets database table: " + cursor.getCount());
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
     }
 }
