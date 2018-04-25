ddec1ce84333fb21f09cc553eef5b73910d9e566
kunaalkumar <kunaal@ashiana.com>
2018-04-25 00:09:02 -0600

    Remove splash screen

--- app/src/main/java/com/ashiana/zlifno/alder/Activity/add/AddImageNoteActivity.java
+++ app/src/main/java/com/ashiana/zlifno/alder/Activity/add/AddImageNoteActivity.java
@@ -12,20 +12,11 @@ import com.ashiana.zlifno.alder.R;
 import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
 
 public class AddImageNoteActivity extends SwipeBackActivity {
-    View rootView;
-    ImageView imageView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_add_image);
-    }
-
-    @Nullable
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.fragment_add_image, container, false);
-        imageView = rootView.findViewById(R.id.imageView);
-        return rootView;
     }
 
 }
