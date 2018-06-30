b608054c7c2e77fff4114eb63cbcd41ed3f99ab6
kunaalkumar <kunaal@ashiana.com>
2018-06-30 09:31:30 -0700

    Optimize imports

--- app/src/main/java/com/ashiana/zlifno/alder/data/Note.java
+++ app/src/main/java/com/ashiana/zlifno/alder/data/Note.java
@@ -1,13 +1,10 @@
 package com.ashiana.zlifno.alder.data;
 
 import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
 import java.io.Serializable;
-import java.util.Calendar;
-import java.util.Date;
 
 // Defines columns to add to database
 @Entity(tableName = "note_table")
